9fc7817ae4722a94269ee720f46e0f887c925953
Seungwoo Lee <timosean@naver.com>
2023-01-31 18:01:03 +0900

    Feat: 로딩될 때의 UI 추가 (react-spinners 사용)

--- src/app/auth/loading.tsx
+++ src/app/auth/loading.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { FadeLoader } from "react-spinners";
+
 export default function Loading() {
-  return <div className="text-3xl text-white font-bold">Loading...</div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <FadeLoader
+        radius={5}
+        loading={true}
+        color={"rgba(255, 255, 255, 0.8"}
+        speedMultiplier={4}
+      />
+    </div>
+  );
 }
