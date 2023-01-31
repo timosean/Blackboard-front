b564620f4c219d925f09ae9270d0865a43bc8d33
Seungwoo Lee <timosean@naver.com>
2023-01-31 22:49:13 +0900

    Fix: SSL issue (HTTPS -> HTTP)

--- src/apis/utils/index.ts
+++ src/apis/utils/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://localhost:8080/api/",
+  baseURL: "http://localhost:8080/api/",
   timeout: 2000,
   withCredentials: true,
 });
