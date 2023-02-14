a9972c7e802759cee56912f45ce1126f4fa10741
Seungwoo Lee <timosean@naver.com>
2023-02-14 12:31:12 +0900

    Revert "Feat: Redux 기본 세팅 적용"
    This reverts commit cb737d304d0ca3cdf90d272ca0bcf75c8b88b60c.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
 import "./globals.css";
-import reducer from "@/reducers/user";
-import { Provider } from "react-redux";
-import withRedux from "next-redux-wrapper";
-import store from "@/store/configureStore";
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <Provider store={store}>
-      <html lang="en">
-        {/*
+    <html lang="en">
+      {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-        <head />
-        <body>{children}</body>
-      </html>
-    </Provider>
+      <head />
+      <body>{children}</body>
+    </html>
   );
-};
-
-export default RootLayout;
+}
