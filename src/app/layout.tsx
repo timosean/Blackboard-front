b5d53c14a1717fd4a0a27a95479d301ac6f9cde8
Seungwoo Lee <timosean@naver.com>
2023-02-16 00:05:40 +0900

    Feat: 모든 페이지에 Redux와 react-query Provider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import "./globals.css";
+import Providers from "@/redux/provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
     <html lang="en">
       {/*
@@ -12,7 +18,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <Providers>
+        <QueryClientProvider client={queryClient}>
+          <body>{children}</body>
+        </QueryClientProvider>
+      </Providers>
     </html>
   );
 }
