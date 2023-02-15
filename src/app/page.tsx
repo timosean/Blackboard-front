5768aa013fc4e3709024aef0ce933a4066d1ad31
Seungwoo Lee <timosean@naver.com>
2023-02-16 00:05:59 +0900

    Feat: 메인페이지/로그인페이지 분기점

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import { fetchMyInfo } from "@/apis/api/user";
+import { useRouter } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Home() {
-  return <main className="w-full flex">메인페이지</main>;
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["user"],
+    queryFn: fetchMyInfo,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data) {
+    queryClient.setQueryData(["user"], data);
+    router.push("/main");
+  }
+
+  return <main className="w-full flex">홈페이지</main>;
 }
