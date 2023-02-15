3868a0d7708569b0bd927b43289a5ad6e2b5656f
Seungwoo Lee <timosean@naver.com>
2023-02-15 14:56:31 +0900

    Form: User interafce에 주석 추가

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,7 +1,9 @@
+// 사용자 정보를 작성할 때 password 필요
+// 사용자 정보를 받아올 때 password 제왼
 export interface User {
-  userName: string;
-  idNum: string;
-  userID: string;
-  password: string;
-  userJob: string;
+  userName?: string;
+  idNum?: string;
+  userID?: string;
+  password?: string; 
+  userJob?: string;
 }
