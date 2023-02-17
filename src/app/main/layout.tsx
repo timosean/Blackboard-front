bb34a05a89f708fb488ed1842cc37590fd4aac68
Seungwoo Lee <timosean@naver.com>
2023-02-17 15:15:47 +0900

    Feat: 사이드바가 계속 고정되도록 레이아웃에 children 추가

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,5 +1,10 @@
 import Sidebar from "@/components/Sidebar";
 
-export default function MenuList() {
-  return <Sidebar />;
+export default function MenuList({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full flex">
+      <Sidebar />
+      <main className="w-full">{children}</main>
+    </div>
+  );
 }
