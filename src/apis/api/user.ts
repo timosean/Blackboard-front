02ff67b1ddb7c87096555503ce1a853db348f62f
Seungwoo Lee <timosean@naver.com>
2023-02-17 13:26:14 +0900

    Refactor: 각 API들에서 에러처리 제외 (React-query에 맡길 예정)

--- src/apis/api/user.ts
+++ src/apis/api/user.ts
@@ -3,20 +3,12 @@ import { User } from "@/interfaces/user";
 
 // 회원가입 API
 export const signUpAPI = async (signUpData: User) => {
-  try {
-    const res = await axiosInstance.post("/user", signUpData);
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const res = await axiosInstance.post("/user", signUpData);
+  return res.data;
 };
 
 // 내 정보 가져오기
 export const fetchMyInfo = async () => {
-  try {
-    const res = await axiosInstance.get("/user");
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
+  const res = await axiosInstance.get("/user");
+  return res.data;
 };
---
+++ src/hooks/queries/user.ts
@@ -0,0 +1,18 @@
+import {
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from "@tanstack/react-query";
+import { fetchMyInfo } from "@/apis/api/user";
+import { User } from "@/interfaces/user";
+import { AxiosError, AxiosResponse } from "axios";
+
+export const useFetchMyInfo = (
+  options: UseQueryOptions<
+    AxiosResponse<User>,
+    AxiosError,
+    AxiosResponse<User>,
+    ["user"]
+  >
+): UseQueryResult<AxiosResponse, AxiosError> =>
+  useQuery(["user"], () => fetchMyInfo(), options);
