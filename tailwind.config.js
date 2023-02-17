19dc51ef88eca95b58d7158e26b29950421acd13
Seungwoo Lee <timosean@naver.com>
2023-02-17 14:16:39 +0900

    Feat: 사이드바 hover 시 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         "ku-beige": "#D7C9B2",
         "ku-crimson": "#7B031D",
+        "hover-crimson": "#5D0012",
         "dark-gray": "#2D282E",
       },
     },
