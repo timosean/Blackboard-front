4f35473549aa2bd90b87817f0fb6f3aeb1811859
Seungwoo Lee <timosean@naver.com>
2023-02-27 17:02:44 +0900

    Design: 강의 컴포넌트 UI 모바일에 맞게 사이즈 수정

--- src/app/main/sugang/components.tsx
+++ src/app/main/sugang/components.tsx
@@ -9,10 +9,14 @@ export const SugangLecture = ({
   professor: string;
 }) => {
   return (
-    <div className="w-[22rem] h-20 flex items-center px-6 justify-between border-l-8 border-l-ku-crimson border bg-white rounded drop-shadow-lg">
+    <div className="w-[22rem] h-14 lg:h-20 flex items-center px-6 justify-between border-l-8 border-l-ku-crimson border bg-white rounded drop-shadow-lg">
       <div className="w-full h-full flex items-center space-x-6">
-        <h1 className="font-bold text-2xl flex items-center">{name}</h1>
-        <h2 className="text-dark-gray font-semibold text-lg">{professor}</h2>
+        <h1 className="font-bold text-xl lg:text-2xl flex items-center">
+          {name}
+        </h1>
+        <h2 className="text-dark-gray h-full flex items-center font-semibold text-base lg:text-lg">
+          {professor}
+        </h2>
       </div>
       <button className="w-16 py-4 h-6 flex items-center justify-center rounded bg-ku-crimson text-white font-bold">
         신청
