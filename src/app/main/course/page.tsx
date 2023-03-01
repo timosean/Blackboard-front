ec7951597fc0e89bd1c84256f4c5fc3572dd111c
Seungwoo Lee <timosean@naver.com>
2023-03-02 00:17:01 +0900

    Feat: 강의 개설 버튼 위치 확정

--- src/app/main/course/page.tsx
+++ src/app/main/course/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { useCheckLoginStatus } from "@/hooks/queries/user";
+import { GoPlus } from "react-icons/go";
+
+function AddCourseButton() {
+  return (
+    <button className="w-30 h-10 px-4 flex items-center space-x-1 rounded bg-ku-crimson text-white font-bold hover:bg-hover-crimson">
+      <GoPlus size={20} />
+      <span>강의 개설</span>
+    </button>
+  );
+}
+
 export default function MyCourse() {
-  return <div className="w-full h-[107px] bg-pink">'코스 페이지'</div>;
+  const user = useCheckLoginStatus();
+
+  return (
+    <div className="w-full">
+      <h1 className="text-3xl h-[105px] flex items-center font-bold border-b-4">
+        나의 코스
+      </h1>
+      <div
+        className={`w-full mt-6 ${
+          user?.userJob === "professor" ? "flex" : "hidden"
+        } items-center justify-start`}
+      >
+        <AddCourseButton />
+      </div>
+    </div>
+  );
 }
